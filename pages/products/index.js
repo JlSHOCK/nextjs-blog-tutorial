364596deadd375e295733703f3349fbd57354bd9
John Shockey <jshockey@mavenlink.com>
2023-02-24 20:14:26 -0800

    update

--- pages/products/index.js
+++ pages/products/index.js
@@ -13,14 +13,29 @@ const Products = ({ products }) => {
       <section>
         <div className={`container`}>
           <h1 className={`title size-h-xxl weight-600`}>Products</h1>
-          <ul className={'blog-items'}>
+          <ul className={'products'}>
             {products.map(product => (
-              <li key={product.slug} className={'blog-item'}>
+              product.name !== undefined &&
+              <li key={product.slug} className={'product'}>
                 <Link href={`${product.link}`}>
-                  <a className={'blog-item-link'} target="_blank">
-                    <span className={'blog-item-title size-h-m weight-300'}>{product.name}</span>
-                  </a>
+                  <a className={'product-link'} target="_blank"></a>
                 </Link>
+                <div className={'product-img-container'}>
+                  <img src={product.image ? product.image : "/images/placeholder-product.png"} alt={product.name} className={`product-img`} />
+                </div>
+                <div className={'product-content'}>
+                  <h3 className={'product-name size-p-l weight-600'}>{product.name}</h3>
+                  {
+                    product.company &&
+                    <p className={'product-company size-p-m weight-300'}>{product.company}</p>
+                  }
+                  {
+                    product.category &&
+                    <div className={'product-categories'}>
+                      <p className={'product-category weight-800'}>{product.category}</p>
+                    </div>
+                  }
+                </div>
               </li>
             ))}
           </ul>
@@ -31,16 +46,14 @@ const Products = ({ products }) => {
 }
 
 export async function getStaticProps() {
-  // List of files in blgos folder
   const filesInProducts = fs.readdirSync('./content/products')
 
-  // Get the front matter and slug (the filename without .md) of all files
   const products = filesInProducts.map(filename => {
     const file = fs.readFileSync(`./content/products/${filename}`, 'utf8')
     const matterData = matter(file)
 
     return {
-      ...matterData.data, // matterData.data contains front matter
+      ...matterData.data,
       slug: filename.slice(0, filename.indexOf('.'))
     }
   })
