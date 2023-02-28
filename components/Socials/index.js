8c27602af78b8050155f8b9cadaaacad275dbc4f
John Shockey <jshockey@mavenlink.com>
2023-02-27 20:45:28 -0800

    update

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -3,41 +3,39 @@ import FacebookIcon from './FacebookIcon';
 import TwitterIcon from './TwitterIcon';
 import LinkedinIcon from './LinkedinIcon';
 import InstagramIcon from './InstagramIcon';
+import YouTubeIcon from './YouTubeIcon';
 import Link from 'next/link';
+import { attributes as ContactContent } from '../../content/contact.md';
 
 const Socials = () => {
-    const socials = [
-        {
-            title: "Facebook",
-            link: "https://www.facebook.com/GoKantata",
-            icon: <FacebookIcon />
-        },
-        {
-            title: "Twitter",
-            link: "https://twitter.com/GoKantata",
-            icon: <TwitterIcon />
-        },
-        {
-            title: "LinkedIn",
-            link: "https://www.linkedin.com/company/gokantata/",
-            icon: <LinkedinIcon />
-        },
-        {
-            title: "Instagram",
-            link: "https://www.instagram.com/teamkantata/",
-            icon: <InstagramIcon />
+    let { socials } = ContactContent;
+
+    const switchIcon = (param) => {
+        switch (param) {
+            case 'Facebook':
+                return <FacebookIcon />;
+            case 'Twitter':
+                return <TwitterIcon />;
+            case 'LinkedIn':
+                return <LinkedinIcon />;
+            case 'Instagram':
+                return <InstagramIcon />;
+            case 'YouTube':
+                return <YouTubeIcon />;
+            default:
+                return null;
         }
-    ];
+    }
 
     return (
         <ul className="social-icons">
             {
-                socials.map(({ title, link, icon }, index) => {
+                socials.map(({ platform, link }, index) => {
                     return (
-                        <li className="social-icon" key={`social-${index}`} title={title}>
+                        <li className="social-icon" key={`social-${index}`} title={platform}>
                             <Link href={link}>
                                 <a className={`social-icon-link`} target="_blank">
-                                    {icon}
+                                    {switchIcon(platform)}
                                 </a>
                             </Link>
                         </li>
