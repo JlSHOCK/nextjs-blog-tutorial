9199e104994f1e46f723c30ddcae35a29922efde
John Shockey <jshockey@mavenlink.com>
2023-03-06 11:21:19 -0800

    Add hero

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Link from "next/link";
 import { attributes as HomeContent } from '../content/home.md';
-console.log(HomeContent)
+
 const Home = () => {
   return (
     <Layout
@@ -10,9 +11,39 @@ const Home = () => {
       description=""
     >
       <section className="hero">
-        {HomeContent && <img src={HomeContent.heroImage ? HomeContent.heroImage : "/images/test.jpg"} className="hero-bg-img" />}
+        <img src={HomeContent.heroImage ? HomeContent.heroImage : "/images/test.jpg"} className="hero-bg-img" />
         <div className={`container`}>
           <h1 className={`title size-h-xxl weight-600`}>{HomeContent ? HomeContent.title : "Hello World"}</h1>
+          {
+            HomeContent.bullets &&
+            <ul className={`bullets`}>
+              {
+                HomeContent.bullets.map((bullet, index) => {
+                  return (
+                    <li className={'bullet size-p-l weight-500'} key={`bullet-${index}`}>• {bullet.bullet}</li>
+                  );
+                })
+              }
+            </ul>
+          }
+          {
+            HomeContent.buttons &&
+            <div className={`cta_btns`}>
+              {
+                HomeContent.buttons.map((button, index) => {
+                  return (
+                    <button className="cta_btn" key={`button-${index}`}>
+                      <Link href={button.ctaLink}>
+                        <a className="cta_btn_link size-p-m weight-800">
+                          {button.ctaTitle}
+                        </a>
+                      </Link>
+                    </button>
+                  );
+                })
+              }
+            </div>
+          }
         </div>
       </section>
     </Layout >
