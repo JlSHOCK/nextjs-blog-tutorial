64d180f994729ebddccd56e56906ba8fb6e0cbec
John Shockey <jshockey@mavenlink.com>
2023-03-08 10:28:23 -0800

    add content

--- components/SignUp/index.js
+++ components/SignUp/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './signup.module.scss';
+import { attributes as SignupContent } from '../../content/signup.md';
 
 const SignUp = () => {
     return (
@@ -7,19 +8,22 @@ const SignUp = () => {
             <div className={`${styles.signup_container} container`}>
                 <div className={`${styles.signup_content}`}>
                     <h3 className={`${styles.title} size-h-l weight-600`}>
-                        Join the Renovating Health Community
+                        {SignupContent.title ? SignupContent.title : "Join the Renovating Health Community"}
                     </h3>
-                    <ul className={`${styles.signup_bullets}`}>
-                        <li className={`${styles.signup_bullet} size-p-s weight-400`}>
-                            1. Receive my free guide Nourish Your Womb.
-                        </li>
-                        <li className={`${styles.signup_bullet} size-p-s weight-400`}>
-                            2. Exclusive deals and offers on products.
-                        </li>
-                        <li className={`${styles.signup_bullet} size-p-s weight-400`}>
-                            3. Get access to our online community.
-                        </li>
-                    </ul>
+                    {
+                        SignupContent.bullets &&
+                        <ul className={`${styles.signup_bullets}`}>
+                            {
+                                SignupContent.bullets.map((bullet, index) => {
+                                    return (
+                                        <li className={`${styles.signup_bullet} size-p-s weight-400`} key={`bullet-${index}`}>
+                                            {index + 1}. {bullet.bullet}
+                                        </li>
+                                    );
+                                })
+                            }
+                        </ul>
+                    }
                 </div>
                 <div className={`${styles.signup_form_container}`}>
 
